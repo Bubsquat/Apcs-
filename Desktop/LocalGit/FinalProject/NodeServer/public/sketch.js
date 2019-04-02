950fd26cbac28a0e165659caab6d1e04b8e3690f
Kaian <416dororo@gmail.com>
2019-04-02 12:19:16 -1000

    Began Player functionality, not currently working

--- Desktop/LocalGit/FinalProject/NodeServer/public/sketch.js
+++ Desktop/LocalGit/FinalProject/NodeServer/public/sketch.js
@@ -8,8 +8,8 @@ let pSize = 60;
 
 function setup() {
   createCanvas(800,800);
-  pX = random((pSize/2)+5,width-((pSize/2)+5));
-  pY = random((pSize/2)+5,height-((pSize/2)+5));
+  pX = parseInt(random((pSize/2)+5,width-((pSize/2)+5)));
+  pY = parseInt(random((pSize/2)+5,height-((pSize/2)+5)));
   socket = io.connect('https://localhost:3000');
 }
 
@@ -18,8 +18,69 @@ var data = {
 	y: pY
 }
 
+// edge detection for player movement 
 
-function keyPressed(){
+function edgeCheck(data, key, pSize){
+
+  var availableMovement = {
+    l:1,
+    r:1,
+    u:1,
+    d:1
+  }
+
+  if(pX+pSize <= 0 && key == 'a'){
+    l = 0;
+  }else{
+    l = 1;
+  }
+
+  if(pX-pSize >= width && key == 'd'){
+    r = 0;
+  }else{
+    r = 1;
+  }
+
+  if(pY+pSize <= 0 && key == 'w'){
+    u = 0;
+  }else{
+    u = 1;
+  }
+
+  if(pY-pSize >= height && key == 's'){
+    d = 0;
+  }else{
+    d = 1;
+  }
+  return availableMovement; 
+
+}
+
+
+function keyPressed(availableMovement){
+
+  //left
+  if(key == 'a'){
+    pX = pX - l;
+  }
+
+  //rights
+  if(key == 'd'){
+    pX = pX + r;
+
+  }
+
+  //up 
+  if(key == 'w'){
+    pY = pY - u;
+  }
+
+  //down 
+  if(key == 's'){
+    pY = pY + d;
+  }
+
+
 	console.log(pX + ',' + pY);
 
 	socket.emit('position', data);
